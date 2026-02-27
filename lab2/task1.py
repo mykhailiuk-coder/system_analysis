ba3da78d38855ff7a71d9a550c26184a612a5e4b
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-02-27 11:40:59 +0200

    Refactor get_state function and update print statements
    Refactor get_state function to accept an array of current states and return a new state array. Update print statements for clarity.

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,13 +1,16 @@
-#correct version
 import numpy as np
 
-def get_state(matrix, current_state_idx, states):
+def get_state(matrix, current_state_arr, states):
+    current_state_idx = current_state_arr.index(1)
     matrix = np.array(matrix)
-    probs = matrix[current_state_idx]
+    probs = matrix @ current_state_arr
     probs = probs / probs.sum()
-    return np.random.choice(states, p=probs)
+    new_state_value = np.random.choice(states, p=probs)
+    new_state = [0] * len(states)
+    new_state[states.index(new_state_value)] = 1
+    return new_state
 
-m = [
+m = [ 
     [0, 1, 0, 0, 0, 0],
     [0.2, 0, 0.8, 0, 0, 0],
     [0, 0.4, 0, 0.6, 0, 0],
@@ -17,14 +20,14 @@ m = [
 ]
 
 all_states = [0, 1, 2, 3, 4, 5]
-current_state = 2  
+current_state = [1, 0, 0, 0, 0, 0]
 
-print(f"Початковий стан: {current_state}")
+print(f"Initial state: {current_state}")
 history = [current_state]
 
 for _ in range(20):
     current_state = get_state(m, current_state, all_states)
     history.append(current_state)
 
-print("Траєкторія:")
-print(" -> ".join(map(str, history)))
+print("Trajectory:")
+print(" ->\n".join(map(str, history)))
