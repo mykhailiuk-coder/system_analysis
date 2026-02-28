a8647e18f0b228e736cb60bbe32053594c257c17
mykhailiuk-coder <mykhailiuk.vitalii@chnu.edu.ua>
2026-02-28 15:24:33 +0200

    Implement user input for state transition simulation
    Added a while loop for user input to select tasks and run simulations for different state transition matrices.

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def get_state(matrix, current_state_arr, states):
-    current_state_idx = current_state_arr.index(1)
     matrix = np.array(matrix)
     probs = matrix @ current_state_arr
     probs = probs / probs.sum()
@@ -10,24 +9,47 @@ def get_state(matrix, current_state_arr, states):
     new_state[states.index(new_state_value)] = 1
     return new_state
 
-m = [ 
-    [0, 1, 0, 0, 0, 0],
-    [0.2, 0, 0.8, 0, 0, 0],
-    [0, 0.4, 0, 0.6, 0, 0],
-    [0, 0, 0.6, 0, 0.4, 0],
-    [0, 0, 0, 0.8, 0, 0.2],
-    [0, 0, 0, 0, 1, 0]
-]
+while True:
+    task = int(input("Input task: "))
+    if task == 1:
+        all_states1 = [0, 1, 2, 3, 4, 5]
+        current_state1 = [1, 0, 0, 0, 0, 0]
+        m1 = [ 
+            [0, 1, 0, 0, 0, 0],
+            [0.2, 0, 0.8, 0, 0, 0],
+            [0, 0.4, 0, 0.6, 0, 0],
+            [0, 0, 0.6, 0, 0.4, 0],
+            [0, 0, 0, 0.8, 0, 0.2],
+            [0, 0, 0, 0, 1, 0]
+        ]
 
-all_states = [0, 1, 2, 3, 4, 5] 
-current_state = [1, 0, 0, 0, 0, 0]
+        print(f"Initial state: {current_state1}")
+        history1 = [current_state1]
 
-print(f"Initial state: {current_state}")
-history = [current_state]
+        for _ in range(20):
+            current_state = get_state(m1, current_state1, all_states1)
+            history1.append(current_state1)
 
-for _ in range(20):
-    current_state = get_state(m, current_state, all_states)
-    history.append(current_state)
+        print("Trajectory:")
+        print(" ->\n".join(map(str, history1)))
+    if task == 2:
+        all_states2 = ["Sunny", "Rainy", "Snowy"]
+        current_state2 = [1, 0, 0]
+        m2 = [
+            [0, 0.5, 0.5],
+            [0.25, 0.5, 0.25],
+            [0.25, 0.25, 0.5]
+        ]
 
-print("Trajectory:")
-print(" ->\n".join(map(str, history)))
+        print(f"Initial state: {current_state2}")
+        history2 = [current_state2]
+
+        for _ in range(20):
+            current_state2 = get_state(m2, current_state2, all_states2)
+            history2.append(current_state2)
+
+        print("Trajectory:")
+        print(" ->\n".join(map(str, history2)))
+
+    else:
+        print("Invalid task number. Please enter 1 to run the simulation.")
